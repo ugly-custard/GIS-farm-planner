9fd456da1894cd1eb4660c80de33a553164e3c27
Adnan Khan <93566182+AdnanKhan-ak47@users.noreply.github.com>
2024-01-06 13:50:15 +0530

    The Bugs in migrate and seeds is fixed, the farm data is now available at the endpoint farms (#5)
    * The Bugs in migrate and seeds is fixed, the farm data is now available in the endpoint /farms
    
    * The data from the /farms endpoint is show by circles on the map

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 import routes from "./routes/index.js";
+import cors from 'cors'
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors())
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/Map.jsx
+++ src/Map.jsx
@@ -1,6 +1,6 @@
 
-import { React, useState } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { React, useEffect, useState } from 'react';
+import { Circle, CircleMarker, MapContainer, TileLayer, Tooltip } from 'react-leaflet'
 import { Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import "./map.css";
@@ -11,9 +11,23 @@ const indiaCenter = [20.5937, 78.9629]; // Center coordinates of India
 const Map = () => {
     const [showMarker, setShowMarker] = useState(true);
 
+    const [farms, setFarms] = useState({})
+
     const handleToggleMarker = () => {
         setShowMarker(!showMarker);
     };
+
+    useEffect(() => {
+        getFarms()
+        console.log(farms)
+    }, [])
+
+    const getFarms = async () => {
+        const farmData = await fetch(`http://localhost:5000/farms`).then(res => res.json())
+        // console(farmData)
+        setFarms(farmData[0])
+    }
+
 
     return (
         <div className='Map__main'>
@@ -35,6 +49,18 @@ const Map = () => {
                             </Popup>
                         </Marker>
                     )}
+
+                    {Object.keys(farms).map((farm) => {
+                        return (
+                            <Circle
+                                key={farm}
+                                center={[farms["Latitude"], farms["Longitude"]]}
+                                pathOptions={{ color: 'red' }}
+                                radius={200}>
+                                <Tooltip>Cover Crop: {farms["CoverCrop"]} | Cover Crop Group: {"CoverCropGroup"} | Grain Crop: {farms["GrainCrop"]} | Grain Crop Group: {"GrainCropGroup"}</Tooltip>
+                            </Circle>
+                        )
+                    })}
 
 
                 </MapContainer >

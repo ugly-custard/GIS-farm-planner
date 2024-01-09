4898ea73971d6fa08916c1099770ab6de50db1cb
Adnan Khan <93566182+AdnanKhan-ak47@users.noreply.github.com>
2024-01-09 17:41:17 +0530

    Feature to draw on map added and the coords of the polygon are output in the console. (#7)
    * The Bugs in migrate and seeds is fixed, the farm data is now available in the endpoint /farms
    
    * The data from the /farms endpoint is show by circles on the map
    
    * Feature to Draw on map added, the coords of the polygon are output in the console

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,9 +1,11 @@
 
 import { React, useEffect, useState } from 'react';
-import { Circle, CircleMarker, MapContainer, TileLayer, Tooltip } from 'react-leaflet'
+import { Circle, FeatureGroup, MapContainer, TileLayer, Tooltip } from 'react-leaflet'
 import { Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
 import "./map.css";
+import 'leaflet/dist/leaflet.css';
+import "leaflet-draw/dist/leaflet.draw.css"
+import { EditControl } from 'react-leaflet-draw';
 
 
 const indiaCenter = [20.5937, 78.9629]; // Center coordinates of India
@@ -28,11 +30,45 @@ const Map = () => {
         setFarms(farmData[0])
     }
 
+    const _onCreated = e => {
+        console.log(e)
+        console.log("Latitude: ",e.layer._latlng.lat)
+        console.log("Longitude: ",e.layer._latlng.lng)
+    }
+
+    const _onEdited = e => {
+        console.log(e)
+        console.log("Latitude: ",e.layer._latlng.lat)
+        console.log("Longitude: ",e.layer._latlng.lng)
+    }
+
+    const _onDeleted = e => {
+        console.log(e)
+        console.log("Latitude: ",e.layer._latlng.lat)
+        console.log("Longitude: ",e.layer._latlng.lng)
+    }
+
 
     return (
         <div className='Map__main'>
-            <div className="map">
-                <MapContainer center={indiaCenter} zoom={5} style={{ height: '400px', width: '100vw' }}>
+            <div className="map" style={{height: '100vh', width: '100vw'}}>
+                <MapContainer center={indiaCenter} zoom={5} style={{ height: '100vh', width: '100vw' }}>
+
+                    <FeatureGroup>
+                        <EditControl
+                            position='topright'
+                            onCreated={_onCreated}
+                            onEdited={_onEdited}
+                            onDeleted={_onDeleted}
+                            draw={{
+                                rectangle: false,
+                                polyline: false,
+                                circlemarker: false,
+                                marker: false
+                            }}
+                        />
+                    </FeatureGroup>
+
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -57,7 +93,7 @@ const Map = () => {
                                 center={[farms["Latitude"], farms["Longitude"]]}
                                 pathOptions={{ color: 'red' }}
                                 radius={200}>
-                                <Tooltip>Cover Crop: {farms["CoverCrop"]} | Cover Crop Group: {"CoverCropGroup"} | Grain Crop: {farms["GrainCrop"]} | Grain Crop Group: {"GrainCropGroup"}</Tooltip>
+                                <Tooltip>Cover Crop: {farms["CoverCrop"]} | Cover Crop Group: {farms["CoverCropGroup"]} | Grain Crop: {farms["GrainCrop"]} | Grain Crop Group: {farms["GrainCropGroup"]}</Tooltip>
                             </Circle>
                         )
                     })}

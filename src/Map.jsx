9104a14b51204a396ae0f83331501e6178926d0d
smit jiwani <smitjiwani44@gmail.com>
2024-01-14 13:46:14 +0530

    marker updates on coordinates, now we gotta add a search bar fot location and reverse engineer that to coordinates...

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,17 +1,24 @@
 
 import { React, useEffect, useState } from 'react';
-import { Circle, FeatureGroup, MapContainer, TileLayer, Tooltip } from 'react-leaflet'
+import { FeatureGroup, MapContainer, TileLayer, Tooltip } from 'react-leaflet'
 import { Marker, Popup } from 'react-leaflet';
 import "./map.css";
 import 'leaflet/dist/leaflet.css';
 import "leaflet-draw/dist/leaflet.draw.css"
 import { EditControl } from 'react-leaflet-draw';
 
+//note: lets not map farms everytime we render the map, lets just map the farms once and then update the map when we add a new farm
+//or another approach could be that we get the farms using lattitude and longitude and then map the farms
+//rn it maps the farm everytime the coordinates are changed very bad for perfomance my computer is lagging....
 
 const indiaCenter = [20.5937, 78.9629]; // Center coordinates of India
 
 const Map = () => {
     const [showMap, setShowMap] = useState(true);
+    const [showMarker, setShowMarker] = useState(true);
+    const [latitude, setLatitude] = useState(20.5937); // [latitude, longitude]
+    const [longitude, setLongitude] = useState(78.9629); // [latitude, longitude]
+    const [coordinates, setCoordinates] = useState([latitude, longitude]); // [latitude, longitude]
 
     const [farms, setFarms] = useState([])
 
@@ -19,10 +26,11 @@ const Map = () => {
         setShowMap(!showMap);
     };
 
-    useEffect(() => {
-        getFarms()
-        console.log(farms)
-    }, [])
+    // useEffect(() => {
+    //     getFarms()
+    //     console.log(farms)
+    // }, [])
+
 
     const getFarms = async () => {
         const farmData = await fetch(`http://localhost:5000/farms`).then(res => res.json())
@@ -48,13 +56,23 @@ const Map = () => {
         console.log("Longitude: ",e.layer._latlng.lng)
     }
 
+    const HandleCoordinates = () => {
+        if (latitude && longitude)
+            setCoordinates([latitude, longitude])
+    }
+
+
+    useEffect(() => {
+        console.log(coordinates)
+        console.log(farms)
+    }, [coordinates])
 
     return (
         <div className='Map__main'>
             <div className="map" style={{ display: 'flex',flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: '90vh'}} >
                 {showMap && 
                 
-                <MapContainer center={indiaCenter} zoom={5} style={{ height: '50vh', width: '50vw' }}>
+                    <MapContainer key={coordinates.toString()} center={coordinates} zoom={5} style={{ height: '50vh', width: '50vw' }}>
 
                     <FeatureGroup>
                         <EditControl
@@ -77,16 +95,15 @@ const Map = () => {
                     />
 
                     {/* Conditional Marker and Popup for India */}
-                    {/* {showMarker && (
-                        <Marker position={indiaCenter}>
+                        {showMarker && (
+                            <Marker position={coordinates}>
                             <Popup>
-                                <div>
-                                    <h3>India</h3>
-                                    <p>This is India!</p>
-                                </div>
+                                    {/* add location name based on coordinates in h2 and coordinates in p */}
+                                    <h2></h2>
+                                    <p>Latitude: {coordinates[0]}, longitude: {coordinates[1]}</p>
                             </Popup>
                         </Marker>
-                    )} */}
+                        )}
 
                     {/* {Object.keys(farms).map((farm) => {
                         return (
@@ -101,8 +118,8 @@ const Map = () => {
                     })} */}
 
                     {/* Had to do this to map the array, Before the endpoint was returning an object and now it returns an array */}
-                    {farms.map((farm) => {
-                        // console.log(farm)
+                    {/* {farms.map((farm) => {
+                        console.log(farm)
                         return (
                             <Marker
                                 key={farm["id"]}
@@ -111,7 +128,7 @@ const Map = () => {
                                 <Tooltip>Cover Crop: {farm["covercrop"]} | Cover Crop Group: {farm["covercropgroup"]} | Grain Crop: {farm["graincrop"]} | Grain Crop Group: {farm["graincropgroup"]}</Tooltip>
                             </Marker>
                         )
-                    })}
+                    })} */}
 
 
                 </MapContainer >
@@ -119,6 +136,16 @@ const Map = () => {
             </div>
             <button onClick={handleToggleMap}>
                 {showMap ? 'Hide Map' : 'Show Map'}
+            </button>
+            <button onClick={() => setShowMarker(!showMarker)}>
+                {showMarker ? 'Hide Marker' : 'Show Marker'}
+            </button>
+
+            <input type="integer" placeholder="Enter Latitude" onChange={(e) => setLatitude(parseFloat(e.target.value))} />
+            <input type="integer" placeholder="Enter Longitude" onChange={(e) => setLongitude(parseFloat(e.target.value))} />
+
+            <button onClick={HandleCoordinates}>
+                click me
             </button>
         </div>
     );

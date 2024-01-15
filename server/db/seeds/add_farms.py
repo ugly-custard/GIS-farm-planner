f6b32957c0f38c74ba2689b8406c6ecf973180e9
Mustafa Akolawala <112169542+MustafaAkolawala@users.noreply.github.com>
2024-01-15 22:37:46 +0530

    New feature (#16)
    * feat: Add base WPI predictor model
    
    Create a base model for predicting the WPI using the data from the
    wholesale Price Index csv file. used linear regression and achieved
    99% accuracy. for now for predicting future WPI index.
    
    * Chore: Change migration table for price index
    
    Changed the attribute names for the price index according
    to the new changed made in the corpus.

--- server/db/seeds/add_farms.py
+++ server/db/seeds/add_farms.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 
 # CSV file path and database connection string
 csv_file_path1 = "/app/data/Soil_data.csv"
-csv_file_path2 = "/app/data/Food_price_indices.csv"
+csv_file_path2 = "/app/data/wholesale_price_indices.csv"
 db_connection_string = "postgresql://username:password@db:5432/username"
 
 # Read CSV file into a Pandas DataFrame
@@ -16,6 +16,6 @@ engine = create_engine(db_connection_string)
 
 # Insert data into the database table (replace 'table_name' with your table name)
 data1.to_sql("farms", engine, if_exists="append", index=False)
-data2.to_sql("food_price_indices", engine, if_exists="append", index=False)
+data2.to_sql("wholesale_price_indices", engine, if_exists="append", index=False)
 
 print("Data successfully seeded into the database.")

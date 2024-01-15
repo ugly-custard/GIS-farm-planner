acc87d4fd4165a09e5d3b890a2feef62eef018c7
Mustafa Akolawala <112169542+MustafaAkolawala@users.noreply.github.com>
2024-01-15 17:49:37 +0530

    Feat: Seed Food_price_indices (#14)
    * Feat: Seed Food_price_indices
    
    seeding data about food price indices and the chane associated with it
    on different types of products like cereals, dairy, meat etc for
    testing and for building the crop price prediction model.
    
    * made a new fikle and it works
    
    ---------
    
    Co-authored-by: smit jiwani <smitjiwani44@gmail.com>

--- server/db/migrations/20231226063348_create_farm_table.js
+++ server/db/migrations/20231226063348_create_farm_table.js
@@ -42,3 +42,26 @@ import db from '../db.js'
     process.exit(1)
   }
 })()
+
+;(async () => {
+  try {
+    await db.schema.dropTableIfExists('price_index')
+    await db.schema.withSchema('public').createTable('price_index', (table) => {
+      table.increments('id').unique().notNullable()
+      table.string('Date').notNullable()
+      table.float('Food_Price_Index').notNullable()
+      table.float('Meat')
+      table.float('Dairy')
+      table.float('Cereals')
+      table.float('Oils')
+      table.float('Sugar')
+      table.timestamps(true, true)
+    })
+    console.log('price index table created!')
+    process.exit(0)
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+})()
+
--- server/db/seeds/add_farms.py
+++ server/db/seeds/add_farms.py
@@ -3,16 +3,19 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # CSV file path and database connection string
-csv_file_path = "/app/data/Soil_data.csv"
+csv_file_path1 = "/app/data/Soil_data.csv"
+csv_file_path2 = "/app/data/Food_price_indices.csv"
 db_connection_string = "postgresql://username:password@db:5432/username"
 
 # Read CSV file into a Pandas DataFrame
-data = pd.read_csv(csv_file_path)
+data1 = pd.read_csv(csv_file_path1)
+data2 = pd.read_csv(csv_file_path2)
 
 # Connect to the database using SQLAlchemy
 engine = create_engine(db_connection_string)
 
 # Insert data into the database table (replace 'table_name' with your table name)
-data.to_sql("farms", engine, if_exists="append", index=False)
+data1.to_sql("farms", engine, if_exists="append", index=False)
+data2.to_sql("food_price_indices", engine, if_exists="append", index=False)
 
 print("Data successfully seeded into the database.")

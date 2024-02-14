1217f5bf911ff70a53cc493803013009fead2574
Mustafa Akolawala <112169542+MustafaAkolawala@users.noreply.github.com>
2024-02-14 14:32:43 +0530

    feat: api endpoint implemented in front end (#30)
    * feat: added plant disease identification model
    
    created a plant disease identifaication model using resnet34, base CNN
    model and VGG16 mmodel.
    
    * chore: minor changes
    
    * feat: create predict disease script
    
    * feat: create corpus and chatbot
    
    created an intent.json file for the intent based chatbot and also
    created the whole chatbot model for the same using nltk toolkit.
    
    * feat: integrate chatbot in backend
    
    created another script to run the chatbot with saved pickle data and
    weights in a class and used that class in app.py to create a post
    request api endpoint to run the chatbot.
    
    * feat: api endpoint
    
    api endpoint for yeild prediction implemented in front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Map from './components/Map'
 import PriceIndexChart from './components/Chart'
-import Signin from './pages/signin'
+// import Signin from './pages/signin'
 import Navbar from './components/Navbar'
 import Dashboard from './pages/Dashboard'
 
--- client/src/components/YeildPredictor.jsx
+++ client/src/components/YeildPredictor.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/YeildPredictor.css"
 import img from "../assets/undraw_flowers_vx06 1.svg"
 import Connect from './Connect';
 
 function YeildPredictor() {
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const body = []
+        Object.keys(parameters).forEach((key, i) => {
+            let value = parameters[key];
+            if(i > 2){
+                value = parseFloat(value);
+            }
+            body.push(value);
+        });
+        console.log(body)
+        const response = await fetch('http://localhost:5000/predict', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        });
+        const res = response.json();
+        console.log(res)
+    }
+
+    const [parameters, setParameters] = useState({
+        crop: '',
+        season: '',
+        state: '',
+        area: '',
+        production: '',
+        annual_rainfall: '',
+        fertilizer: '',
+        pesticide: '',
+    })
+
+    const onChange = (e) => {
+        console.log(parameters)
+        setParameters({ ...parameters, [e.target.name]: e.target.value });
+    }
+
     const cropOptions = {
         'Maize': 0,
         'Sugarcane': 1,
@@ -25,18 +64,18 @@ function YeildPredictor() {
                 <img src={img} alt="" />
                 <div className='YeildPredictor__form'>
                 <h2>Fill Out the Details Manually</h2>
-                <form className='YeildPredictorForm'>
+                <form className='YeildPredictorForm' onSubmit={handleSubmit}>
                     <label htmlFor="crop" className="yieldPrediction__form__label">Crop:</label>
-                    <select id="crop" name="crop" className="yeildPrediction__form__select">
+                    <select onChange={onChange} value={parameters.crop} id="crop" name="crop" className="yeildPrediction__form__select">
                         {Object.keys(cropOptions).map((crop) => (
-                            <option key={cropOptions[crop]} value={cropOptions[crop]}>
+                            <option key={cropOptions[crop]} value={crop}>
                                 {crop}
                             </option>
                         ))}
                     </select>
 
                     <label htmlFor="season" className="yieldPrediction__form__label">Season:</label>
-                    <select id="season" name="season" className="yeildPrediction__Form__Select">
+                    <select onChange={onChange} value={parameters.season} id="season" name="season" className="yeildPrediction__Form__Select">
                         <option value="summer">Summer</option>
                         <option value="winter">Winter</option>
                         <option value="autumn">Autumn</option>
@@ -44,7 +83,7 @@ function YeildPredictor() {
                     </select>
 
                     <label htmlFor="state" className="yieldPrediction__form__label">State:</label>
-                    <select id="state" name="state" className="yeildPrediction__Form__Select">
+                    <select onChange={onChange} value={parameters.state} id="state" name="state" className="yeildPrediction__Form__Select">
                         <option value="Andhra Pradesh">Andhra Pradesh</option>
                         <option value="Arunachal Pradesh">Arunachal Pradesh</option>
                         <option value="Assam">Assam</option>
@@ -76,21 +115,21 @@ function YeildPredictor() {
                     </select>
 
                     <label htmlFor="area" className="yieldPrediction__form__label">Area:</label>
-                    <input type="text" id="area" name="area" className="yeildPrediction__Form__Select" />
+                    <input onChange={onChange} value={parameters.area} type="text" id="area" name="area" className="yeildPrediction__Form__Select" />
 
                     <label htmlFor="production" className="yieldPrediction__form__label">Production:</label>
-                    <input type="text" id="production" name="production" className="yeildPrediction__Form__Select" />
+                    <input onChange={onChange} value={parameters.production} type="text" id="production" name="production" className="yeildPrediction__Form__Select" />
 
                     <label htmlFor="annual_rainfall" className="yieldPrediction__form__label">Annual Rainfall:</label>
-                    <input type="text" id="annual_rainfall" name="annual_rainfall" className="yeildPrediction__Form__Select" />
+                    <input onChange={onChange} value={parameters.annual_rainfall} type="text" id="annual_rainfall" name="annual_rainfall" className="yeildPrediction__Form__Select" />
 
                     <label htmlFor="fertilizer" className="yieldPrediction__form__label">Fertilizer:</label>
-                    <input type="text" id="fertilizer" name="fertilizer" className="yeildPrediction__Form__Select" />
+                    <input onChange={onChange} value={parameters.fertilizer} type="text" id="fertilizer" name="fertilizer" className="yeildPrediction__Form__Select" />
 
                     <label htmlFor="pesticide" className="yieldPrediction__form__label">Pesticide:</label>
-                    <input type="text" id="pesticide" name="pesticide" className="yeildPrediction__Form__Select" />
+                    <input onChange={onChange} value={parameters.pesticide} type="text" id="pesticide" name="pesticide" className="yeildPrediction__Form__Select" />
 
-                    <button>Submit</button>
+                    <button type='submit' >Submit</button>
                 </form>
                 </div>
                 

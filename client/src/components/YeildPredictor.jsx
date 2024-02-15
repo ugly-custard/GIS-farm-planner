7fcd6298a45eccc90db75003e17d20bfe6461096
Adnan Khan <93566182+AdnanKhan-ak47@users.noreply.github.com>
2024-02-15 13:15:10 +0530

    API for the yield predictor is connected , now the UI remains to be added for the result (#31)
    * The Bugs in migrate and seeds is fixed, the farm data is now available in the endpoint /farms
    
    * The data from the /farms endpoint is show by circles on the map
    
    * Feature to Draw on map added, the coords of the polygon are output in the console
    
    * Fixed the mapping problem of the farm data fetched
    
    * The coords funcitonality added again
    
    * UI kinda fixed for the map
    
    * Map is shortened and the API to get 3 nearest location is implemented
    
    * Showing only the nearest coords of the polygon now
    
    * The price index chart is fixed and is now working
    
    * What did i do?
    
    * Pulling changes
    
    * Added Flask
    
    * API connected for the yield predictor

--- client/src/components/YeildPredictor.jsx
+++ client/src/components/YeildPredictor.jsx
@@ -10,13 +10,13 @@ function YeildPredictor() {
         const body = []
         Object.keys(parameters).forEach((key, i) => {
             let value = parameters[key];
-            if(i > 2){
+            if (i > 2) {
                 value = parseFloat(value);
             }
             body.push(value);
         });
         console.log(body)
-        const response = await fetch('http://localhost:5000/predict', {
+        const response = await fetch('http://localhost:8080/api/yield-prediction', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -43,19 +43,9 @@ function YeildPredictor() {
         setParameters({ ...parameters, [e.target.name]: e.target.value });
     }
 
-    const cropOptions = {
-        'Maize': 0,
-        'Sugarcane': 1,
-        'Cotton': 2,
-        'Tobacco': 3,
-        'Paddy': 4,
-        'Barley': 5,
-        'Wheat': 6,
-        'Millets': 7,
-        'Oil seeds': 8,
-        'Pulses': 9,
-        'Ground Nuts': 10
-    };
+    const cropOptions = { "Arecanut": 0, "Arhar/Tur": 1, "Castor seed": 2, "Coconut": 3, "Cotton(lint)": 4, "Dry chillies": 5, "Gram": 6, "Jute": 7, "Linseed": 8, "Maize": 9, "Mesta": 10, "Niger seed": 11, "Onion": 12, "Potato": 13, "Rapeseed &Mustard": 14, "Rice": 15, "Sesamum": 16, "Small millets": 17, "Sugarcane": 18, "Sweet potato": 19, "Tapioca": 20, "Tobacco": 21, "Turmeric": 22, "Wheat": 23, "Bajra": 24, "Black pepper": 25, "Cardamom": 26, "Coriander": 27, "Garlic": 28, "Ginger": 29, "Groundnut": 30, "Horse-gram": 31, "Jowar": 32, "Ragi": 33, "Cashewnut": 34, "Banana": 35, "Soyabean": 36, "Barley": 37, "Khesari": 38, "Masoor": 39, "Moong(Green Gram)": 40, "Safflower": 41, "Sannhamp": 42, "Sunflower": 43, "Urad": 44, "Peas & beans (Pulses)": 45, "other oilseeds": 46, "Other Cereals": 47, "Cowpea(Lobia)": 48, "Oilseeds total": 49, "Guar seed": 50, "Moth": 51 };
+    const seasonOptions = { "Whole Year": 0, "Kharif": 1, "Rabi": 2, "Autumn": 3, "Summer": 4, "Winter": 5 };
+    const stateOptions = { "Assam": 0, "Karnataka": 1, "Kerala": 2, "Meghalaya": 3, "West Bengal": 4, "Puducherry": 5, "Goa": 6, "Andhra Pradesh": 7, "Tamil Nadu": 8, "Odisha": 9, "Bihar": 10, "Gujarat": 11, "Madhya Pradesh": 12, "Maharashtra": 13, "Mizoram": 14, "Punjab": 15, "Uttar Pradesh": 16, "Haryana": 17, "Himachal Pradesh": 18, "Tripura": 19, "Nagaland": 20, "Chhattisgarh": 21, "Uttarakhand": 22, "Jharkhand": 23, "Delhi": 24, "Manipur": 25, "Jammu and Kashmir": 26, "Telangana": 27, "Arunachal Pradesh": 28, "Sikkim": 29 }
 
     return (
         <div className='YeildPredictor'>
@@ -63,78 +53,56 @@ function YeildPredictor() {
             <div className="YeildPredictorBottom">
                 <img src={img} alt="" />
                 <div className='YeildPredictor__form'>
-                <h2>Fill Out the Details Manually</h2>
-                <form className='YeildPredictorForm' onSubmit={handleSubmit}>
-                    <label htmlFor="crop" className="yieldPrediction__form__label">Crop:</label>
-                    <select onChange={onChange} value={parameters.crop} id="crop" name="crop" className="yeildPrediction__form__select">
-                        {Object.keys(cropOptions).map((crop) => (
-                            <option key={cropOptions[crop]} value={crop}>
-                                {crop}
-                            </option>
-                        ))}
-                    </select>
+                    <h2>Fill Out the Details Manually</h2>
+                    <form className='YeildPredictorForm' onSubmit={handleSubmit}>
+                        <label htmlFor="crop" className="yieldPrediction__form__label">Crop:</label>
+                        <select onChange={onChange} value={parameters.crop} id="crop" name="crop" className="yeildPrediction__form__select">
+                            {Object.keys(cropOptions).map((crop) => (
+                                <option key={cropOptions[crop]} value={crop}>
+                                    {crop}
+                                </option>
+                            ))}
+                        </select>
 
-                    <label htmlFor="season" className="yieldPrediction__form__label">Season:</label>
-                    <select onChange={onChange} value={parameters.season} id="season" name="season" className="yeildPrediction__Form__Select">
-                        <option value="summer">Summer</option>
-                        <option value="winter">Winter</option>
-                        <option value="autumn">Autumn</option>
-                        <option value="spring">Spring</option>
-                    </select>
+                        <label htmlFor="season" className="yieldPrediction__form__label">Season:</label>
+                        <select onChange={onChange} value={parameters.season} id="season" name="season" className="yeildPrediction__Form__Select">
+                            {Object.keys(seasonOptions).map((season) => (
+                                <option key={seasonOptions[season]} value={season}>
+                                    {season}
+                                </option>
+                            ))}
+                        </select>
 
-                    <label htmlFor="state" className="yieldPrediction__form__label">State:</label>
-                    <select onChange={onChange} value={parameters.state} id="state" name="state" className="yeildPrediction__Form__Select">
-                        <option value="Andhra Pradesh">Andhra Pradesh</option>
-                        <option value="Arunachal Pradesh">Arunachal Pradesh</option>
-                        <option value="Assam">Assam</option>
-                        <option value="Bihar">Bihar</option>
-                        <option value="Chhattisgarh">Chhattisgarh</option>
-                        <option value="Goa">Goa</option>
-                        <option value="Gujarat">Gujarat</option>
-                        <option value="Haryana">Haryana</option>
-                        <option value="Himachal Pradesh">Himachal Pradesh</option>
-                        <option value="Jharkhand">Jharkhand</option>
-                        <option value="Karnataka">Karnataka</option>
-                        <option value="Kerala">Kerala</option>
-                        <option value="Madhya Pradesh">Madhya Pradesh</option>
-                        <option value="Maharashtra">Maharashtra</option>
-                        <option value="Manipur">Manipur</option>
-                        <option value="Meghalaya">Meghalaya</option>
-                        <option value="Mizoram">Mizoram</option>
-                        <option value="Nagaland">Nagaland</option>
-                        <option value="Odisha">Odisha</option>
-                        <option value="Punjab">Punjab</option>
-                        <option value="Rajasthan">Rajasthan</option>
-                        <option value="Sikkim">Sikkim</option>
-                        <option value="Tamil Nadu">Tamil Nadu</option>
-                        <option value="Telangana">Telangana</option>
-                        <option value="Tripura">Tripura</option>
-                        <option value="Uttar Pradesh">Uttar Pradesh</option>
-                        <option value="Uttarakhand">Uttarakhand</option>
-                        <option value="West Bengal">West Bengal</option>
-                    </select>
+                        <label htmlFor="state" className="yieldPrediction__form__label">State:</label>
+                        <select onChange={onChange} value={parameters.state} id="state" name="state" className="yeildPrediction__Form__Select">
+                            {Object.keys(stateOptions).map((state) => (
+                                <option key={stateOptions[state]} value={state}>
+                                    {state}
+                                </option>
+                            ))}
+                        </select>
 
-                    <label htmlFor="area" className="yieldPrediction__form__label">Area:</label>
-                    <input onChange={onChange} value={parameters.area} type="text" id="area" name="area" className="yeildPrediction__Form__Select" />
+                        <label htmlFor="area" className="yieldPrediction__form__label">Area:</label>
+                        <input onChange={onChange} value={parameters.area} type="text" id="area" name="area" className="yeildPrediction__Form__Select" />
 
-                    <label htmlFor="production" className="yieldPrediction__form__label">Production:</label>
-                    <input onChange={onChange} value={parameters.production} type="text" id="production" name="production" className="yeildPrediction__Form__Select" />
+                        <label htmlFor="production" className="yieldPrediction__form__label">Production:</label>
+                        <input onChange={onChange} value={parameters.production} type="text" id="production" name="production" className="yeildPrediction__Form__Select" />
 
-                    <label htmlFor="annual_rainfall" className="yieldPrediction__form__label">Annual Rainfall:</label>
-                    <input onChange={onChange} value={parameters.annual_rainfall} type="text" id="annual_rainfall" name="annual_rainfall" className="yeildPrediction__Form__Select" />
+                        <label htmlFor="annual_rainfall" className="yieldPrediction__form__label">Annual Rainfall:</label>
+                        <input onChange={onChange} value={parameters.annual_rainfall} type="text" id="annual_rainfall" name="annual_rainfall" className="yeildPrediction__Form__Select" />
 
-                    <label htmlFor="fertilizer" className="yieldPrediction__form__label">Fertilizer:</label>
-                    <input onChange={onChange} value={parameters.fertilizer} type="text" id="fertilizer" name="fertilizer" className="yeildPrediction__Form__Select" />
+                        <label htmlFor="fertilizer" className="yieldPrediction__form__label">Fertilizer:</label>
+                        <input onChange={onChange} value={parameters.fertilizer} type="text" id="fertilizer" name="fertilizer" className="yeildPrediction__Form__Select" />
 
-                    <label htmlFor="pesticide" className="yieldPrediction__form__label">Pesticide:</label>
-                    <input onChange={onChange} value={parameters.pesticide} type="text" id="pesticide" name="pesticide" className="yeildPrediction__Form__Select" />
+                        <label htmlFor="pesticide" className="yieldPrediction__form__label">Pesticide:</label>
+                        <input onChange={onChange} value={parameters.pesticide} type="text" id="pesticide" name="pesticide" className="yeildPrediction__Form__Select" />
 
-                    <button type='submit' >Submit</button>
-                </form>
+                        <button type='submit' >Submit</button>
+                    </form>
                 </div>
-                
+
             </div>
-            
+
         </div>
     )
 }
--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 from cropRecommender.crpRcmndr import CropRecommender
 from fertilizerDetection.fertilizer import FertilizerRecommender
@@ -6,6 +7,7 @@ from yieldPredictor.yieldPredictor import YieldPredictor
 from chatbot.run import Chatbot
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 fertilizer_recommender = FertilizerRecommender()
 crop_recommender = CropRecommender()
@@ -18,7 +20,7 @@ def hello_world():
     return "<p>Hello, Poosay!</p>"
 
 
-@app.route("/crop-recommendation", methods=["POST"])
+@app.route("/api/crop-recommendation", methods=["POST"])
 def crop_recommendation():
     data = request.json
     crop = crop_recommender.predict(data)
@@ -26,7 +28,7 @@ def crop_recommendation():
     return jsonify({"recommended_crop": crop})
 
 
-@app.route("/yield-prediction", methods=["POST"])
+@app.route("/api/yield-prediction", methods=["POST"])
 def yield_prediction():
     data = request.json
     predicted_yield = yield_predictor.predict(data)
@@ -34,14 +36,14 @@ def yield_prediction():
     return jsonify({"predicted_yield": predicted_yield})
 
 
-@app.route("/fertilizer-recommendation", methods=["POST"])
+@app.route("/api/fertilizer-recommendation", methods=["POST"])
 def fertilizer_recommendation():
     data = request.json
     prediction = fertilizer_recommender.predict(data)
 
     return jsonify({"predicted_fertilizer": prediction})
 
-@app.route('/chatbot', methods=['POST'])
+@app.route('/api/chatbot', methods=['POST'])
 def get_response():
     data = request.get_json()
     query = data['query']
@@ -50,4 +52,4 @@ def get_response():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=8080)

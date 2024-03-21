1411134bcbd3d3b06643497083c3ead5b5057f71
Mustafa Akolawala <112169542+MustafaAkolawala@users.noreply.github.com>
2024-03-21 17:24:17 +0530

    yield predicts (#41)

--- server/yieldPredictor/yieldPredictor.py
+++ server/yieldPredictor/yieldPredictor.py
@@ -32,9 +32,21 @@ class YieldPredictor:
         data["State"] = data["State"].map(self.labels[2])
 
         prediction = self.model.predict(self.ct.transform(data))[0]
-        response = self.client.chat.completions.create(
-            model="gpt-3.5-turbo-0125",
-            response_format={"type": "json_object"},
-            prompt=f"Based on the input parameters:\n{datacp.iloc[0].values.flatten().tolist()}\nand the predicted yield of {prediction} kg/acre, here are some personalized suggestions to increase your yield:\n",
-        )
-        return prediction, response.choices[0].message.content
+        # response = self.client.chat.completions.create(
+        #     model="gpt-3.5-turbo-0125",
+        #     response_format={"type": "json_object"},
+        #     messages=[
+        #         {"role": "system", "content": "You are a helpful farming assistant."},
+        #         {"role": "user", "content": f"Based on the input parameters:\n{datacp.iloc[0].values.flatten().tolist()}\nand the predicted yield of {prediction} kg/acre, here are some personalized suggestions to increase your yield:\n"},
+        #     ]
+        # )
+        message = """\
+    To optimize crop yield, select suitable crops based on climate and soil conditions, \
+    time planting and harvesting appropriately, adhere to state-specific agricultural \
+    guidelines, manage land efficiently, monitor production levels for timely intervention, \
+    employ irrigation to supplement rainfall, conduct soil tests for precise fertilizer \
+    application, implement integrated pest management for effective pest control, and \
+    continually adjust practices based on real-time data. These strategies, coupled with \
+    staying informed about advancements, will maximize yield and enhance agricultural productivity.\
+    """
+        return prediction, message # response.choices[0].message.content
